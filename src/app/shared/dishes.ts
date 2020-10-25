1816c94767747f69a518370fb7a9bb9f24d85e11
Mehmet Sökmen <sokmen4083@gmail.com>
2020-10-25 18:13:19 +0100

    Sprachkommission Style

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -3,10 +3,10 @@ import { Dish } from './dish';
 export const DISHES: Dish[] = [
   {
     id: '0',
-    name: 'Sprachkommission',
-    image: '/assets/images/sprechen-komision.jpg',
+    name: 'Deutchkurs',
+    image: '/assets/images/deutchkurs.jpg',
     // tslint:disable-next-line:max-line-length
-    description: 'Als Verein Verfolgt Sprachkommission produzieren wir seit Anfang 2019 Projekte und setzen das Sprachenlernen systematisch um.',
+    description: '',
     comments: [
       {
         rating: 5,
@@ -14,16 +14,75 @@ export const DISHES: Dish[] = [
         author: "Mehmet Dogan",
         date: "2020-08-15T17:57:28.556094Z"
       },
+      {
+        rating: 5,
+        comment: "Vielen Dank für kurse.",
+        author: "Mukaddes Sökmen",
+        date: "2020-10-16T17:57:28.556094Z"
+      },
+      
+      {
+        rating: 5,
+        comment: "Danke Schön",
+        author: "Ali Ögretmen",
+        date: "2020-09-22T17:57:28.556094Z"
+      },
 
     ],
 
   },
   {
     id: '1',
-    name: 'Kurse',
-    image: '/assets/images/kurs.jpg',
+    name: 'Französisch Kurs',
+    image: '/assets/images/französischkurs.jpg',
     // tslint:disable-next-line:max-line-length
-    description: 'Als Verein Verfolgt Sprachkommission produzieren wir seit Anfang 2019 Projekte und setzen das Sprachenlernen systematisch um.',
+    description: '',
+    comments: [
+      {
+        rating: 5,
+        comment: "Vielen Dank für kurse.",
+        author: "Mukaddes Sökmen",
+        date: "2020-10-16T17:57:28.556094Z"
+      },
+      
+      {
+        rating: 5,
+        comment: "Danke Schön",
+        author: "Ali Ögretmen",
+        date: "2020-09-22T17:57:28.556094Z"
+      },
+    ],
+
+  },
+  {
+    id: '2',
+    name: 'Italienischkurs',
+    image: '/assets/images/italienischkurs.jpg',
+    // tslint:disable-next-line:max-line-length
+    description: '',
+    comments: [
+      {
+        rating: 5,
+        comment: "Vielen Dank für kurse.",
+        author: "Mukaddes Sökmen",
+        date: "2020-10-16T17:57:28.556094Z"
+      },
+      
+      {
+        rating: 5,
+        comment: "Danke Schön",
+        author: "Ali Ögretmen",
+        date: "2020-09-22T17:57:28.556094Z"
+      },
+    ],
+
+  },
+  {
+    id: '3',
+    name: 'Türkischkurs',
+    image: '/assets/images/türkischkurs.jpg',
+    // tslint:disable-next-line:max-line-length
+    description: '',
     comments: [
       {
         rating: 5,
